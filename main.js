235c07a7114cb8f1c2a30f3378a86113d0f54d82
datiashvilinika <nikushadtia24@gmail.com>
2025-02-23 15:05:55 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+const name = "nika"
+const age = 15
+const num1 = 7
+const num2 = 17
+const num3 = 28 + 15
+const num4 = 28 - 15
+const num5 = 5 * 6
+const num6 = 20 / 5
+console.log(num6)
+console.log(num3)
+const num7 = num1 + num2
+console.log(num7)
+const num8 = num1 * num2
+console.log(num8)
+const hh1 = document.querySelector(".hh1")
+console.log(hh1)
+hh1.style.color = "red"
+hh1.style.border = "2px solid black"
+hh1.style.backgroundColor = "pink"
+hh1.style.fontSize = "70PX"
+hh1.remove()
